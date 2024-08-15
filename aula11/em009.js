9b2fe7be7003ff26b30e110824c8657df119a8d0
Ester Oliveira <168388277+esteroliveira04@users.noreply.github.com>
2024-08-15 11:23:47 -0300

    att

---
+++ aula11/em009.js
@@ -0,0 +1,8 @@
+var país = 'brasil'
+console.log(`vivendo em ${país}`)
+if (país == 'brasil'){
+    console.log('voce é brasileiro')
+}else{
+    console.log('voce é estrangeiro')
+}
+
---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 81
+console.log(`seu carro está a ${vel} km/h`)
+if (vel > 80){ //condiçao simples
+    console.log('voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('use sempre o sinto de seguraça')
